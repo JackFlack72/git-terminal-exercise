6b5d2ee9ee587cedb0c33cefb7ffbc9386725198
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-11-26 12:24:34 -0600

    initial commit

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,5 +1,5 @@
 package com.pluralsight;
-
+// test commit comment
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
